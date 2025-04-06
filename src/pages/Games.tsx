648f90aa2f6e5d3932bcc55906ec0de37528ebc8
Sidhant Pandey <sidhantpandeynp6@gmail.com>
2025-04-06 23:25:06 +0530

    * Change Games page * Add debounce hook

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,13 +1,17 @@
 import CanvasGame from './CanvasGame.tsx';
+import Whiteboard from './WhiteBoard.tsx';
 
 const Games = () => {
   return (
-    <div className="min-h-screen pt-16">
-      <section className="py-20 bg-gray-900 text-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <CanvasGame />
-        </div>
-      </section>
+    // <div className="min-h-screen pt-16">
+    //   <section className="py-20 bg-gray-900 text-white">
+    //     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    //       <CanvasGame />
+    //     </div>
+    //   </section>
+    // </div>
+    <div className="min-h-screen pt-16" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <Whiteboard />
     </div>
   );
 };
